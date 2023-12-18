eafc81c929cc4968848e75c0719b2922c1b0118c
Nolhan <nolhan.dev@gmail.com>
2023-12-18 18:07:58 +0100

    Updated app url

--- blindtest-vuejs/src/assets/config.js
+++ blindtest-vuejs/src/assets/config.js
@@ -1,5 +1,5 @@
 export default {
   api: "https://blindtest-api.discordanalytics.xyz",
-  website: "https://https://blind-test.vercel.app/",
+  website: "https://blind-test.vercel.app",
   ws: "wss://blindtest-api.discordanalytics.xyz",
 }
